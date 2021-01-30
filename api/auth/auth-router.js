88c3cd23e688cf0771ef68a75c53486557c8f9fa
corey-power <CPower1248@gmail.com>
2021-01-30 18:45:57 -0500

    Auth-router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,4 +2,18 @@ const router = require("express").Router()
 
 const Users = require("../users/model")
 
+// server.post("/register", async (req, res, next) => {
+  // const { credentials } = req.body
+
+  // if (!isValid(credentials)) {
+  //   res.status(400).json({ 
+  //     error: "username, password and role required",
+  //     message: err.message, 
+  //     stack: err.stack
+  //   })
+  // } else {
+
+  // }
+// })
+
 module.exports = router
